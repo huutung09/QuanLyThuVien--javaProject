4d3f2862f07bb748b82095ab7365d5d1f26a5c9a
Hoang Giang <HVGiang86@gmail.com>
2023-02-18 11:40:58 +0700

    tt

--- src/model/DocGia.java
+++ src/model/DocGia.java
@@ -63,6 +63,9 @@ public class DocGia {
     public void setGioiTinh(String gioiTinh) {
         this.gioiTinh = gioiTinh;
     }
+    public void setGioiTinh1(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
 
     public String getPhieuMuonId() {
         return phieuMuonId;
@@ -81,6 +84,10 @@ public class DocGia {
             return true ;
         }
         return false ;
+    }
+
+    public String seminargit() {
+        return phieuMuonId;
     }
 
 }
